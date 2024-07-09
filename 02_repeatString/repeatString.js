8777d0765a21e59da5df39d9875ba67d0e8ad7d9
barryweb <167048463+barryweb@users.noreply.github.com.>
2024-07-09 20:20:38 +1000

    test pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if(num < 0) {
 
-};
+        return "ERROR";
+    }
+
+    let greet = '';
+
+    for (let i = 0; i < num; i++) {
+
+        greet += string;
+    }
+    
+    return greet;
+}
+
+console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
