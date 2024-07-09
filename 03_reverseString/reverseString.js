bb71d8d57531010f6522c0dc173e70bdc4d5a3d2
barryweb <167048463+barryweb@users.noreply.github.com.>
2024-07-09 21:48:24 +1000

    test pass

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    
+    let splitString = str.split("");
 
+    let reverseArray = splitString.reverse();
+
+    let joinArray = reverseArray.join("");
+
+    return joinArray;
 };
-
+reverseString("hello there");
 // Do not edit below this line
 module.exports = reverseString;
