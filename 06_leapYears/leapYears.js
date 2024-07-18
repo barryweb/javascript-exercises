e2626e1e1bf0a624fc6eb3155eef990109d7c3b7
barryweb <167048463+barryweb@users.noreply.github.com.>
2024-07-18 19:38:10 +1000

    test pass 18.07.2024

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const yearDivisibleByFour = year % 4 === 0;
+    const century = year % 100 === 0;
+    const yearDivisibleByFourHundred = year % 400 === 0;
 
+    if ( yearDivisibleByFour && (!century || yearDivisibleByFourHundred))
+    {
+        return true;
+    }
+    else 
+    {
+        return false;
+    }
 };
 
 // Do not edit below this line
