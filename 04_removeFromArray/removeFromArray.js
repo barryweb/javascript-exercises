2f26049340384e88937bbcc81f4d22c790b11baa
barryweb <167048463+barryweb@users.noreply.github.com.>
2024-07-18 18:24:18 +1000

    test pass 18/07/2024

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...args) {
+    return arr.filter(value => !args.includes(value));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
