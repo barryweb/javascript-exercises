86b90924e158abc1f6d5c5f63a79b5535b26993b
barryweb <167048463+barryweb@users.noreply.github.com.>
2025-01-21 21:17:37 +1100

    exercise-completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let splitString = str.split("");
+    let reverseArr = splitString.reverse();
+    let joinArr = reverseArr.join("");
+    return joinArr;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
