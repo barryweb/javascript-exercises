2bf6dcd4ef470072770066b594e6c0552b09d595
barryweb <167048463+barryweb@users.noreply.github.com.>
2025-01-21 19:49:31 +1100

    exercise-complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if (number < 0) {
+        return "ERROR";
+    }
+    if (string === '') {
+        return string;
+    }
+    return string.repeat(number);
 };
-
+// console.log(repeatString("hey", 3));
 // Do not edit below this line
 module.exports = repeatString;
