3d28a912024e1f783cb9e52198059dab9865aa9d
barryweb <167048463+barryweb@users.noreply.github.com.>
2025-01-25 23:08:32 +1100

    exercise-complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const newArr = [];
+    for (const elem of array) {
+        if (!args.includes(elem)) {
+            newArr.push(elem);
+        }
+    }
+    return newArr;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
