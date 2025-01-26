7dcaefc2ed618be68a03fa706a3f8daa13d6439c
barryweb <167048463+barryweb@users.noreply.github.com.>
2025-01-26 14:36:12 +1100

    exercise-complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(startNum, endNum) {
+    if (!Number.isInteger(startNum) || !Number.isInteger(endNum)) {
+        return "ERROR";
+    }
+    if (startNum < 0 || endNum < 0) {
+        return "ERROR";
+    }
+    if (startNum > endNum) {
+        const x = startNum;
+        startNum = endNum;
+        endNum = x;
+    }
+    
+    let finalSum = 0;
+    for (let i = startNum; i <= endNum; i++) {
+        finalSum += i;
+    }
+    return finalSum;
+}
 // Do not edit below this line
 module.exports = sumAll;
